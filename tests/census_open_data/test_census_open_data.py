0939af9c69aee21fb727230096b6b792d5f95655
Charles <charles.gauvin@lacapitale.com>
2022-12-20 09:27:51 -0500

    feat: added basic function to return qc city bounding box as dict + implemente test

--- tests/census_open_data/test_census_open_data.py
+++ tests/census_open_data/test_census_open_data.py
@@ -1,7 +1,18 @@
 from os.path import join, isfile
 import geopandas as gpd
 
-from geo_py_utils.census_open_data.open_data import download_qc_city_neighborhoods
+
+from geo_py_utils.census_open_data.open_data import download_qc_city_neighborhoods, get_qc_city_bbox
  
 def test_qc_city_download():
     download_qc_city_neighborhoods()
+
+def test_qc_city_bbox():
+    dict_bbox = get_qc_city_bbox()
+
+    assert dict_bbox['min_lat'] < dict_bbox['max_lat']
+    assert dict_bbox['min_lng'] < dict_bbox['max_lng']
+
+
+if __name__== "__main__":
+    test_qc_city_bbox()
