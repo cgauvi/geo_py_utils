44c1a44cc8dbfe9e03146d3580f574f691eee488
Charles <charles.gauvin@lacapitale.com>
2023-02-18 12:39:29 -0500

    fix: debugged the qc administrative boundary download

--- geo_py_utils/census_open_data/open_data.py
+++ geo_py_utils/census_open_data/open_data.py
@@ -6,6 +6,7 @@ from os.path import join, exists
 from ben_py_utils.misc.cache import Cache_wrapper
  
 from geo_py_utils.misc.constants import DATA_DIR
+from geo_py_utils.etl.download_zip import download_zip_shp
 
 QC_CITY_NEIGH_URL = "https://www.donneesquebec.ca/recherche/dataset/5b1ae6f2-6719-46df-bd2f-e57a7034c917/resource/436c85aa-88d9-4e57-9095-b72b776a71a0/download/vdq-quartier.geojson"
 
@@ -49,7 +50,7 @@ class DownloadQcAdmBoundaries:
 
     QC_PROV_ADM_BOUND_MRC = "mrc_s"
     QC_PROV_ADM_BOUND_MUNI = "munic_s"
-    QC_PROV_ADM_BOUND_METRO = "comet_s'"
+    QC_PROV_ADM_BOUND_METRO = "comet_s"
 
  
     def __init__(self,
@@ -61,20 +62,17 @@ class DownloadQcAdmBoundaries:
         self.data_download_path = data_download_path
 
 
-    def _download_qc_administrative_boundaries_wrappe(self):
+    def _download_qc_administrative_boundaries(self):
 
         """Download the zip file to disk.
         """
 
-        # Unzip the entire directory of shp files
-        try:
-            download_zip_shp(
-                url = DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_URL,
-                data_download_path = self.data_download_path
+        # Unzip the entire directory of shp files - but dont try to read it in 
+        download_zip_shp(
+                url=DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_URL,
+                data_download_path=self.data_download_path,
+                return_shp_file=False
             )
-        except Exception as e :
-            pass
-    
 
  
     def get_qc_administrative_boundaries(self) -> gpd.GeoDataFrame :
@@ -82,14 +80,16 @@ class DownloadQcAdmBoundaries:
         """
 
         # point to data path of unzipped folder
-        path_shp = join(DATA_DIR, 'qc_adm_regions', 'SHP','Sda', 'version_courante','SHP', f'{self.geo_level}].shp')
+        path_shp = join(DATA_DIR, 'qc_adm_regions', 'SHP','Sda', 'version_courante','SHP', f'{self.geo_level}.shp')
 
         # Unzip if required
         if not exists(path_shp):
-            self._download_qc_administrative_boundaries_wrappe()
+            self._download_qc_administrative_boundaries()
+
+        assert exists(path_shp)
 
         # Read in the desired polygons
-        shp_boundary = gpd.read_file()
+        shp_boundary = gpd.read_file(path_shp)
 
         return shp_boundary
         
@@ -97,82 +97,90 @@ class DownloadQcAdmBoundaries:
   
 
 class DownloadQcDissolvedMrc(DownloadQcAdmBoundaries):
+        
+    """Get the dissolved 17 regions representing the 'usual' Quebec administrative boundaries
+
+    Inherits from DownloadQcAdmBoundaries
+
+    Attributes:
+        data_download_path (_type_, optional): _description_. Defaults to join(DATA_DIR, 'qc_adm_regions').
+    """
+
+    MRC_DISSOLVE_ID_COL = "MRS_NM_REG"  # 'typical' names we are used to seeing: correspond to the keys in DICT_MAPPING
+
+    DICT_MAPPING={
+                "Abitibi-Témiscamingue":    None,
+                "Bas-Saint-Laurent": None,
+                "Capitale-Nationale":  "QUÉBEC" ,
+                "Centre-du-Québec":  "CENTRE-DU-QUÉBEC", 
+                "Chaudière-Appalaches":  "CHAUDIÈRE-APPALACHES", 
+                "Côte-Nord": "CÔTE-NORD", 
+                "Estrie": "ESTRIE", 
+                "Gaspésie--Îles-de-la-Madeleine": "GASPÉSIE/ÎLES-DE-LA-MADELAINE", # watch out with the names ... some characters are different from raw data when reading in with fiona
+                "Lanaudière" :  "LANAUDIÈRE",
+                "Laurentides": "LAURENTIDES"  ,
+                "Laval":"LAVAL" ,
+                "Mauricie":  "MAURICIE/BOIS-FRANCS" ,
+                "Montréal": "MONTRÉAL" ,  
+                "Montérégie":  "MONTÉRÉGIE" , 
+                "Nord-du-Québec": None,
+                "Outaouais": "OUTAOUAIS" ,
+                "Saguenay--Lac-Saint-Jean" :  "SAGUENAY LAC ST-JEAN" # also watch out for -- here - like Iles de la Madelaine above
+        }
 
     def __init__(self,
                 data_download_path=join(DATA_DIR, 'qc_adm_regions')
                 ) :
 
-        super.__init__(DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MRC , data_download_path)
+
+        super().__init__(DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MRC , data_download_path)
                     
 
     def _create_dict_mapping(self) -> dict:
             """Create the hardcoded dict to map values from the official boundary files to the opus data
             """
 
-            DICT_MAPPING={
-                "Abitibi-Témiscamingue": None,
-                    "Bas-Saint-Laurent": None,
-                    "Capitale-Nationale":  "QUÉBEC",
-                    "Centre-du-Québec":  "CENTRE-DU-QUÉBEC",
-                    "Chaudière-Appalaches":  "CHAUDIÈRE-APPALACHES",
-                    "Côte-Nord": "CÔTE-NORD",
-                    "Estrie": "ESTRIE",
-                    "Gaspésie - Îles-de-la-Madeleine": "GASPÉSIE/ÎLES-DE-LA-MADELAINE",
-                    "Lanaudière" :  "LANAUDIÈRE",
-                    "Laurentides": "LAURENTIDES",
-                    "Laval":"LAVAL",
-                    "Mauricie":  "MAURICIE/BOIS-FRANCS",
-                    "Montréal": "MONTRÉAL",
-                    "Montérégie":  "MONTÉRÉGIE",
-                    "Nord-du-Québec": None,
-                    "Outaouais": "OUTAOUAIS",
-                    "Saguenay - Lac-Saint-Jean":  "SAGUENAY LAC ST-JEAN"
-                }
-
             df_mapping = pd.DataFrame.\
-                from_dict(DICT_MAPPING, orient='index').\
+                from_dict(DownloadQcDissolvedMrc.DICT_MAPPING, orient='index').\
                 reset_index()
 
             df_mapping.columns = ['MRS_NM_REG', 'DOMAINE_OPUS']
 
             return df_mapping
 
+    @Cache_wrapper(path_cache=join(DATA_DIR, "cache", "qc_adm_regions_MRC_DISSOLVED_17.parquet"))
+    def get_qc_administrative_boundaries(self) -> gpd.GeoDataFrame:
+        """Take the MRC polygons and dissolve them into the large regions we want (based on `MRS_NM_REG`)
 
-        def get_qc_administrative_boundaries(self) -> gpd.GeoDataFrame:
-            """Take the MRC polygons and dissolve them into the large regions we want (based on `MRS_NM_REG`)
-
-            Dissolving is useful since there are arguably too many details in the dataset
-            For instance, there are many sub polygons associated with 'Cote-Nord', many of whom have the "Nouveau toponyme à venir" label (MRS_NM_MRC column) 
+        Dissolving is useful since there are arguably too many details in the dataset
+        For instance, there are many sub polygons associated with 'Cote-Nord', many of whom have the "Nouveau toponyme à venir" label (MRS_NM_MRC column) 
                 -> it is not extremelly useful to keep all these precise polygons
 
-            Args:
-                self (_type_): _description_
-            Returns:
-                gpd.GeoDataFrame: dissolve geodf
-            """
+        Args:
+            self (_type_): _description_
+        Returns:
+            gpd.GeoDataFrame: dissolve geodf
+        """
 
-            # point to data path of unzipped folder
-            path_shp = join(DATA_DIR, 'qc_adm_regions', 'SHP','Sda', 'version_courante','SHP', 
-                            f'{self.geo_level}.shp')
+        # Call the parent method to download the raw polygons
+        shp_mrc_raw = super().get_qc_administrative_boundaries()
 
-            # Unzip if required
-            if not exists(path_shp):
-                self._download_qc_administrative_boundaries_wrappe()
-
-            # Dissolve according to the large region names: MRS_NM_REG 
-            shp_mrc_dissolved = ss[['MRS_NM_REG','geometry']].\
-                        dissolve(by='MRS_NM_REG').\
+        # Dissolve according to the large region names: MRS_NM_REG 
+        shp_mrc_dissolved = shp_mrc_raw[[DownloadQcDissolvedMrc.MRC_DISSOLVE_ID_COL,'geometry']].\
+                        dissolve(by=DownloadQcDissolvedMrc.MRC_DISSOLVE_ID_COL).\
                         reset_index()
 
-            # Map the opus values
-            dict_mapping = self._create_dict_mapping()
-            shp_mrc_dissolved.merge(df_mapping, on='MRS_NM_REG')
+        # Map the opus values
+        df_mapping = self._create_dict_mapping()
+        shp_mrc_dissolved_merged = shp_mrc_dissolved.merge(df_mapping, on=DownloadQcDissolvedMrc.MRC_DISSOLVE_ID_COL)
 
-            return shp_mrc_dissolved
+        return shp_mrc_dissolved_merged
 
 
 if __name__ == '__main__':
 
-     qc_boundaries_extracter = DownloadQcAdmBoundaries()
+    qc_boundaries_extracter = DownloadQcAdmBoundaries(geo_level = DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MRC )
+    shp_mrc_raw = qc_boundaries_extracter.get_qc_administrative_boundaries()
 
-     shp_mrc = qc_boundaries_extracter.get_dissolved_mrc()
+    qc_mrc_dissolved_extracter = DownloadQcDissolvedMrc()
+    shp_mrc = qc_mrc_dissolved_extracter.get_qc_administrative_boundaries()
--- tests/census_open_data/test_census_open_data.py
+++ tests/census_open_data/test_census_open_data.py
@@ -1,12 +1,14 @@
 from os.path import join, isfile
 import geopandas as gpd
-
+import numpy as np
 
 from geo_py_utils.census_open_data.open_data import (
     download_qc_city_neighborhoods, 
     get_qc_city_bbox,
-    DownloadQcAdmBoundaries
+    DownloadQcAdmBoundaries,
+    DownloadQcDissolvedMrc
 )
+
  
 def test_qc_city_download():
     download_qc_city_neighborhoods()
@@ -18,19 +20,23 @@ def test_qc_city_bbox():
     assert dict_bbox['min_lng'] < dict_bbox['max_lng']
 
 
-def test_mrcs_dssolved():
-    qc_boundaries_extracter = DownloadQcAdmBoundaries()
-    shp_mrc = qc_boundaries_extracter.get_dissolved_mrc() 
+def test_mrcs_dissolved():
+
+    qc_mrc_dissolved_extracter = DownloadQcDissolvedMrc()
+    shp_mrc = qc_mrc_dissolved_extracter.get_qc_administrative_boundaries()
 
     assert shp_mrc.shape[0] == 17
+
+    assert np.all(np.isin([DownloadQcDissolvedMrc.MRC_DISSOLVE_ID_COL, 'DOMAINE_OPUS'], shp_mrc.columns))
 
 
 def test_qa_adm_boundaries():
 
+    # For some reason the number of features is different bepending on the precision - 1/20 or 1/100
     dict_check_num_records = {
             DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_METRO: 2,
-            DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MRC: 148,
-            DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MUNI: 2457
+            DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MRC: 106,
+            DownloadQcAdmBoundaries.QC_PROV_ADM_BOUND_MUNI: 1347
     }
     
     dict_results_num_records = { 
@@ -43,9 +49,10 @@ def test_qa_adm_boundaries():
     diffs = [ abs(dict_results_num_records[k] - dict_check_num_records[k]) \
             for k in dict_check_num_records.keys()]
 
-    assert max(diffs) == 0
+    assert max(diffs) == 0, f"Fatal error, max difference is { max(diffs)}"
 
 
 
 if __name__== "__main__":
-    test_qc_city_bbox()
+    test_mrcs_dissolved()
+    test_qa_adm_boundaries()
