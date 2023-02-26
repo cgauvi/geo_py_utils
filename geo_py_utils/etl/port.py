fa802ea42ae1891ed315dea66ecf571589ad763e
Charles <charles.gauvin@lacapitale.com>
2023-02-26 13:26:18 -0500

    refactor: pot int or string

--- geo_py_utils/etl/port.py
+++ geo_py_utils/etl/port.py
@@ -1,14 +1,15 @@
 import socket
 import logging
+from typing import Union
 
 logger = logging.getLogger(__file__)
 
-def is_port_open(host="127.0.0.1": str, port=5052: int) -> bool:
+def is_port_open(host: str="127.0.0.1", port: Union[str, int]=5052) -> bool:
     """Check if port is open on given host
 
     Args:
-        host (_type_, optional): _description_. Defaults to "127.0.0.1":str.
-        port (_type_, optional): _description_. Defaults to 5052:int.
+        host (str, optional): _description_. Defaults to "127.0.0.1"
+        port (Union[str, int], optional): _description_. Defaults to 5052
 
     Returns:
         bool: True if port open
@@ -16,13 +17,13 @@ def is_port_open(host="127.0.0.1": str, port=5052: int) -> bool:
 
     a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    location = (host, port)
+    location = (host, int(port))
     result_of_check = a_socket.connect_ex(location)
 
     if result_of_check == 0:
-        logger.info("Port is open")
+        logger.info(f"Port {str(port)} is open")
     else:
-        logger.info("Port is closed")
+        logger.info(f"Port {str(port)} is closed")
  
     a_socket.close()
 
